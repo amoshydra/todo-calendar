cca6389c1428d1524d4c4d8814eeca26e15036a6
Amos Wong <amoswwj@gmail.com>
2018-02-06 01:36:52 +0800

    Make insert break existing task

--- src/libs/Commander.js
+++ src/libs/Commander.js
@@ -1,10 +1,35 @@
 import gaHelper from '@/libs/gaHelper/index';
+import store from '@/store';
 
 const Commander = () => {};
 Commander.prototype.execute = ({ action, task }) => {
   switch (action) {
     case 'insert': {
-      return gaHelper.events.insert('primary', task);
+      return store.dispatch('events/retrieve')
+      .then(() => {
+        const currentEvent = store.getters['events/getCurrent'];
+
+        const updatePromises = [
+          gaHelper.events.insert('primary', task),
+        ];
+
+        if (currentEvent) {
+          updatePromises.push(
+            gaHelper.events.update(
+              'primary',
+              currentEvent.id,
+              {
+                ...currentEvent,
+                end: {
+                  dateTime: new Date().toISOString(),
+                }
+              }
+            )
+          );
+        }
+
+        return Promise.all(updatePromises);
+      });
     }
     case 'update': {
       return gaHelper.events.update('primary', task);
