40dd0ebbd1c3b3f91fb4e48fff3ea5c225cb2ffc
Amos Wong <amoswwj@gmail.com>
2018-02-06 01:19:44 +0800

    Expand event store to support retrieval and getter

--- src/store/modules/events.js
+++ src/store/modules/events.js
@@ -1,13 +1,57 @@
+import gaHelper from '@/libs/gaHelper/index';
+
 export default {
   namespaced: true,
-  state: {},
+  state: {
+    [-1]: [],
+    0: [],
+    1: [],
+  },
   getters: {
-    list(state) {
-      return state;
+    /**
+     * Find the first currently occuring event
+     * @param {State} state
+     * @returns closest past event. null if not available
+     */
+    getCurrent(state) {
+      const now = new Date();
+      return state[0].find((event) => {
+        const eventStart = new Date(event.start.dateTime);
+        const eventEnd = new Date(event.end.dateTime);
+        return eventStart < now && now < eventEnd;
+      });
     },
+
+    /**
+     * Find closest past event
+     * @param {State} state
+     * @returns closest past event. null if not available
+     */
+    getClosestPast(state) {
+      const now = new Date();
+      const justPastedEventIndex = state[0].findIndex((event) => {
+        const eventStart = new Date(event.start.dateTime);
+        return eventStart > now;
+      });
+
+      const closestPastIndex = justPastedEventIndex - 1;
+      return state[0][closestPastIndex] || null;
+    }
   },
   mutations: {
+    update(state, { offset, events }) {
+      state[offset] = events;
+    },
   },
   actions: {
+    retrieve({ commit }, offset = 0) {
+      return gaHelper.events.list('primary', offset)
+      .then((events) => {
+        commit('update', {
+          offset,
+          events,
+        });
+      });
+    },
   },
 };
