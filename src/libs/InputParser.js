2d6974bd5c295afb07286bb3de9b57e77f9421d0
Amos Wong <amoswwj@gmail.com>
2018-02-06 00:53:19 +0800

    Update parse to support break, join and push

--- src/libs/InputParser.js
+++ src/libs/InputParser.js
@@ -1,22 +1,52 @@
-import moment from 'moment';
 import Task from '@/libs/Task';
+
+const SHOULD_MAKE_TASK_REGEX = /insert/;
 
 const InputParser = () => {};
 
-InputParser.prototype.parse = (input) => {
-  let action = null;
-  let task = null;
+const makeWordRegex = (array) => {
+  const delimeteredWords = array.join('|');
+  return new RegExp(`^(${delimeteredWords})$`);
+};
 
-  if (input.includes('add ')) {
-    const taskname = input.replace('add ', '');
-    task = new Task(taskname, {
-      start: {
-        dateTime: moment().add(1, 'minutes').format() },
-      end: {
-        dateTime: moment().add(1, 'hours').format() },
-    });
-    action = 'insert';
+const wordMapper = {
+  break: makeWordRegex([
+    'stop',
+    'pause',
+    'halt',
+    'break',
+  ]),
+  join: makeWordRegex([
+    'join',
+    'continue',
+    'resume',
+  ]),
+  push: makeWordRegex([
+    'push',
+  ]),
+};
+
+const mapAction = (input) => {
+  switch (true) {
+    case wordMapper.break.test(input):
+      return 'break';
+    case wordMapper.join.test(input):
+      return 'join';
+    case wordMapper.push.test(input):
+      return 'push';
+    case input.length > 0:
+      return 'insert';
+    default:
+      return null;
   }
+};
+
+InputParser.prototype.parse = (rawInput) => {
+  const input = rawInput.trim();
+
+  const action = mapAction(input);
+  const task = SHOULD_MAKE_TASK_REGEX.test(action) ? new Task(input) : null;
+
   return { action, task };
 };
 
