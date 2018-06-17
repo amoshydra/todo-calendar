4ee434e6d0ddb1a874c7b4ff7fa43e24a8ae6e5e
Amos Wong <amoswwj@gmail.com>
2018-06-17 23:41:50 +0800

    Style login page

--- n-o/layouts/default.vue
+++ n-o/layouts/default.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
+  <main class="main">
     <nuxt/>
-  </div>
+  </main>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Kalam|Montserrat');
+* {
+  font-family: Montserrat, "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+}
+
 html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -14,6 +18,7 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+  background-color: #fbfbfb;
 }
 
 *, *:before, *:after {
@@ -21,32 +26,9 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+main.main {
+  padding: 16px;
+  max-width: 1440px;
+  margin: auto;
 }
 </style>
--- n-o/pages/login.vue
+++ n-o/pages/login.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>
+  <div class="page__login">
+
+    <div class="page__login__intro">
+      <h1 class="brand__title">To-do Calendar</h1>
+      <h4 class="brand__tagline">Where To-do meet calendar</h4>
+    </div>
+
     <button
       @click="login"
+      class="login__button"
     >
-      Login
+      <span>Get started with your Google Calendar</span>
     </button>
   </div>
 </template>
@@ -18,6 +25,65 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+
+.page__login__intro {
+  font-size: 2em;
+  margin-bottom: 120px;
+  padding-top: 32px;
+}
+
+.brand__title {
+  font-family: 'Montserrat', sans-serif;
+  line-height: 1em;
+  margin-bottom: 0.5em;
+}
+.brand__tagline {
+  font-family: 'Kalam', cursive;
+  font-weight: 900;
+  opacity: 0.4;
+  line-height: 1.2em;
+}
+
+.login__button {
+  /* Reset button */
+  border: none;
+  font: inherit;
+  outline: none;
+
+  max-width: 280px;
+  cursor: pointer;
+  color: #1e1e1e;
+  background-color: #FEFEFE;
+  padding: 0.75em 1.25em;
+  box-shadow: 0px 0px 1px 0 rgba(0,0,0,0.1);
+  transition:
+    box-shadow 0.25s,
+    transform 0.25s,
+    background-color 0.1s;
+    ;
+}
+.login__button:focus,
+.login__button:hover {
+  box-shadow: 0px 12px 24px 0 rgba(0,0,0,0.1);
+  transform: translateY(-4px);
+  background-color: #47ff86;
+}
+
+.login__button:active {
+  transform: translateY(-3px);
+  box-shadow: 0px 02px 20px 0 rgba(0,0,0,0.1);
+}
+.login__button span {
+  display: inline-block;
+  text-align: left;
+  line-height: 1.5em;
+  font-size: 1.25em;
+  font-weight: 900;
+  user-select: none;
+  transition:
+    transform 0.1s,
+    ;
+}
 
 </style>
