49e5ac46197c3c7a537793428a0a78c5897a1c85
Amos Wong <amoswwj@gmail.com>
2018-06-18 00:42:21 +0800

    Add full calendar

---
+++ n-o/components/index/calendar-view-wrapper.vue
@@ -0,0 +1,45 @@
+<template>
+  <full-calendar
+    :events="fcEvents"
+    :config="fcConfig"
+  ></full-calendar>
+</template>
+
+<script>
+import { FullCalendar } from 'vue-full-calendar'
+import 'fullcalendar/dist/fullcalendar.css';
+
+export default {
+  components: {
+    FullCalendar,
+  },
+  props: {
+    events: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      fcConfig: {
+        defaultView: 'agendaDay',
+        weekends: false,
+      },
+    };
+  },
+  computed: {
+    fcEvents() {
+      return this.events
+        .map(({ summary, start, end }) => ({
+          title: summary,
+          start: start.dateTime,
+          end: end.dateTime,
+        }));
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
--- n-o/pages/index.vue
+++ n-o/pages/index.vue
@@ -1,15 +1,35 @@
 <template>
   <section class="container">
     <div>
-
-
+      <input>
+    </div>
+    <div>
+      <calendar-view-wrapper
+        :events="$store.state.calendar.events"
+      />
     </div>
   </section>
 </template>
 
 <script>
+import CalendarViewWrapper from "@/components/index/calendar-view-wrapper";
+
 export default {
+  components: {
+    CalendarViewWrapper
+  },
   middleware: 'auth',
+  mounted() {
+    this.refreshCalendarEventList();
+  },
+  methods: {
+    refreshCalendarEventList() {
+      this.$store.dispatch('calendar/list', {
+        startOffset: -7,
+        endOffset: 1,
+      })
+    },
+  }
 }
 </script>
 
