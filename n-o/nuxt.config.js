7f4930e431b277a94f4740dba221c6c7ef92f07c
Amos Wong <amoswwj@gmail.com>
2018-06-18 08:39:27 +0800

    Update title

--- n-o/nuxt.config.js
+++ n-o/nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'n-o',
+    title: 'Todo Calendar',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
