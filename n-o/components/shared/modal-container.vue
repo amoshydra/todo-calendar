34c20948fb4b8f8bb2fd599b11145420e905ec75
Amos Wong <amoswwj@gmail.com>
2018-07-05 02:01:10 +0800

    Make modal z-index same with bootstrap

--- n-o/components/shared/modal-container.vue
+++ n-o/components/shared/modal-container.vue
@@ -52,19 +52,21 @@ export default {
 }
 
 .modal__overlay {
+
   background: rgba(0,0,0,0.2);
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  z-index: 2;
   display: flex;
   justify-content: center;
   align-items: center;
 
   text-align: initial;
   font-size: 1rem;
+
+  z-index: 1030;
 
   animation-name: fadeIn;
   animation-duration: 0.2s;
