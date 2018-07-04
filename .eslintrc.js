d4284f981e2c425e3a2ab9612176371f26ba672b
Amos Wong <amoswwj@gmail.com>
2018-07-05 04:10:18 +0800

    Restore eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,5 +17,72 @@ module.exports = {
     'vue'
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      'optionalDependencies': ['test/unit/index.js']
+    }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    "indent": [
+      "error", 2,
+      { "SwitchCase": 1 }
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "no-console": "warn",
+    "linebreak-style": "off",
+    "max-len": [
+      "warn", 120, 2,
+      { ignoreComments: true }
+    ],
+    "quote-props": [
+      "warn",
+      "consistent-as-needed"
+    ],
+    "no-cond-assign": [
+      "off",
+      "except-parens"
+    ],
+    "radix": "off",
+    "space-infix-ops": "off",
+    "no-use-before-define": "off",
+    "no-unused-vars": [
+      "warn",
+      {
+        "vars": "local",
+        "args": "none",
+        "argsIgnorePattern": "next"
+      }
+    ],
+    "default-case": "error",
+    "no-else-return": "off",
+    "no-param-reassign": "off",
+    "space-before-function-paren": [
+      "warn",
+      {
+        "anonymous": "never",
+        "named": "never",
+        "asyncArrow": "ignore"
+      }
+    ],
+    "eqeqeq": ["error", "smart"],
+    "comma-dangle": "off",
+    "func-names": "warn",
+    "prefer-arrow-callback": "warn",
+    "object-shorthand": "warn",
+    "prefer-const": "warn",
+    "arrow-body-style": "warn",
+  }
 }
