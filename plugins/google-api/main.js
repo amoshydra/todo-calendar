4294cf4adc0e1fe83acc27cca54ec285fe216dbe
Amos Wong <amoswwj@gmail.com>
2018-09-22 00:04:12 +0800

    Revert "Insert env variable outside of async code"
    This reverts commit 18fda7d4d0b5749681ea0123e4ba72d3f9da4385.

--- plugins/google-api/main.js
+++ plugins/google-api/main.js
@@ -1,18 +1,13 @@
 import getGAPI from './core';
 import config from './config';
 
-const environmentKeys = {
-  GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
-  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-};
-
 const gapiPromise = getGAPI()
   .then(gapi => new Promise((resolve) => {
     gapi.load(
       'client:auth2',
       () => gapi.client.init({
-        apiKey: environmentKeys.GOOGLE_API_KEY,
-        clientId: environmentKeys.GOOGLE_CLIENT_ID,
+        apiKey: process.env.GOOGLE_API_KEY,
+        clientId: process.env.GOOGLE_CLIENT_ID,
         discoveryDocs: config.DISCOVERY_DOCS,
         scope: config.SCOPES.join(' '),
       })
