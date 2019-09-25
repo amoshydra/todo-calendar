7d00c005acae133b38b051eb0c940372a5dc562c
Amos Wong <amoswwj@gmail.com>
2019-09-26 00:04:28 +0800

    feat: Dedicate /login/callback/ for oauth callback

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,7 @@ module.exports = {
       redirect: {
         login: '/login',
         logout: '/login',
-        callback: '/login/',
+        callback: '/login/callback/',
         home: '/',
       },
     }],
